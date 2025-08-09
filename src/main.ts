b3b04bbea28b1405a5b8b75510dc75aa223470fd
Hoang Anh <anhh3642@gmail.com>
2025-08-10 00:41:23 +0700

    Connect database(MongoDB)

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+require('dotenv').config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT');
+  // await app.listen(process.env.PORT);
+  await app.listen(port);
+}
+bootstrap();
